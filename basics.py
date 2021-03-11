1b56d78a53069cd75f563e89e1f39234f0d70d32
99003734 <78858575+99003734@users.noreply.github.com>
2021-03-11 07:00:35 +0530

    commited the file basic.py from windows using pycharm

---
+++ basics.py
@@ -0,0 +1,15 @@
+temp = 20
+temp2 = "Shaik"
+print(temp)
+print(type(temp))
+print(id(temp))
+print(temp2[0:2])
+print(temp2[3])
+
+
+a,b,c = 10,15,29
+print(a)
+print(5**3)
+print(5**3**2)
+print("String"*3)
+print(4*'2')
